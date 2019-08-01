1f7ee7cebf0a9cbfa58d8af0f50c03a857166d38
jesyikaturner <jesyika@gmail.com>
2019-08-01 16:57:59 +1000

    commenting

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -9,10 +9,10 @@ public class BoardManager : MonoBehaviour {
     public Attacker playerAttacker;
 
     // Constants
-    private const int BOARD_WIDTH = 6, BOARD_HEIGHT = 6;
-    private const float BOARD_SPACING = 1f; // the distance between the tiles
-    private const float PIECE_OFFSET = 0.1f; // height above the tile that pieces rest
-    private const int max_moves = 2;
+    private const int BOARD_WIDTH = 6, BOARD_HEIGHT = 6; // Size of the board
+    private const float BOARD_SPACING = 1f; // Distance between the tiles
+    private const float PIECE_OFFSET = 0.1f; // Distance above the tile that pieces rest
+    private const int max_moves = 2; // Total moves each player can do
 
     private readonly int[,] PLAYER_ONE_COORDS = { { 0, 0 }, { 0, 1 }, { 0, 2 } };
     private readonly int[,] PLAYER_TWO_COORDS = { { 5, 3 }, { 5, 4 }, { 5, 5 } };
@@ -46,7 +46,9 @@ public class BoardManager : MonoBehaviour {
         {
             for (int z = 0; z < BOARD_WIDTH; z++)
             {
+                // Takes in x value, z value, the offset and the name of the object
                 Tile placeTile = CreatePlaceTile(x, z, 0, "Place");
+                // Setting the object's parent to a child of this script's gameobject.
                 placeTile.transform.parent = transform.GetChild(0).transform;
                 placeTile.xPos = x;
                 placeTile.zPos = z;
@@ -58,19 +60,24 @@ public class BoardManager : MonoBehaviour {
     // Sets up the positions of the players hands.
     private void SetupPlayerHands()
     {
+        // Holds the player hands.
         handHandlers = new List<PlayerHand>
         {
             gameObject.AddComponent<PlayerHand>(),
             gameObject.AddComponent<PlayerHand>()
         };
 
+        // Takes in the hand coords, the tile object, x offset, offset is multiplied by this float, the name of the objects, the player id.
         handHandlers[0].PopulatePlayerHand(PLAYER_ONE_COORDS, placement, -2, BOARD_SPACING, "PLAYER 1 HAND", 1);
+        // Fills the player's hand with random tiles.
         handHandlers[0].FillHand();
         handHandlers[1].PopulatePlayerHand(PLAYER_TWO_COORDS, placement, 2, BOARD_SPACING, "PLAYER 2 HAND", 2);
         handHandlers[1].FillHand();
     }
 
-    //applies a preset layout to the board
+    // Applies a preset layout to the board
+    // this needs to be less specific and more expandable. so i can input different board layouts with different amounts
+    // of base tiles and block tiles or special tiles
     private void SetupBoardLayout()
     {
         for (int i = 0; i < BOARD_LAYOUT.GetLength(0); i++)
@@ -105,6 +112,7 @@ public class BoardManager : MonoBehaviour {
             }
         }
     }
+
 
     private Tile CreatePlaceTile(int x, int z, int offset, string name)
     {
